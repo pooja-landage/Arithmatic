471564aa96f5fde94d9319c54d4c26a2b77e5362
pooja-landage <poojalandage22@gmail.com>
2021-09-16 07:40:12 +0530

    Add project files.

---
+++ Pratice4Arithmatic/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Pratice4Arithmatic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int a, b, c;
+
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of a" + a);
+            
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of b" + b);
+
+
+            a = 34;
+            b = 43;
+
+            // Addition
+             c = a + b;
+             a += b;
+             Console.WriteLine("The sum of a and b is " + c);
+            float div = 24;
+            float result = div / 12;
+            Console.WriteLine("The division is " + result);
+
+
+
+
+        }
+    }
+}
